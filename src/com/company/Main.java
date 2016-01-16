b4e6de0f1c801d03f6ea92ae2767eb559d1ac743
DanielKaczorowski <email@daniel.com.pl>
2016-01-16 14:22:37 +0100

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,38 @@
 package com.company;
 
-/**
- * Szyfr Cezara do ci¹gów znaku.
- */
 public class Main {
 
+    static String caesar(String value, int shift) {
+        // Convert to char array.
+        char[] buffer = value.toCharArray();
+
+        // Loop over characters.
+        for (int i = 0; i < buffer.length; i++) {
+
+            // Shift letter, moving back or forward 26 places if needed.
+            char letter = buffer[i];
+            letter = (char) (letter + shift);
+            if (letter > 'z') {
+                letter = (char) (letter - 26);
+            } else if (letter < 'a') {
+                letter = (char) (letter + 26);
+            }
+            buffer[i] = letter;
+        }
+        // Return final string.
+        return new String(buffer);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        String a = "java";
+        System.out.println(a);
+        System.out.println();
+
+        String b = caesar(a, 2);
+        String c = caesar(b, -2);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println();
     }
 }
